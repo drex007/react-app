e253f5ce0f7029bf3e5a123788236df3e5b545a0
drex007 <vykeoj@gmail.com>
2022-07-01 07:45:53 -0700

    initial

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,6 +9,8 @@ const Home = () => {
     return (  
         <div className="home">
             <BlogList blogs = {blogs} title="All Blogs" />
+            <h1>----------------------------------------</h1>
+            <BlogList blogs = {blogs.filter((blog)=> blog.author ==='mario')} title="Mario's Blogs" />
         </div>
     );
 }
