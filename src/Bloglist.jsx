f6e147b88cef18a5c5e8268782d26182e16878f3
drex007 <vykeoj@gmail.com>
2022-07-01 09:49:18 -0700

    conditional_loading

--- src/Bloglist.jsx
+++ src/Bloglist.jsx
@@ -1,7 +1,5 @@
 const BlogList = ({blogs, title}) => {
-    // const blogs = props.blogs;
-    // const title = props.title;
-    // console.log(props, blogs);
+ 
     return ( 
         <div className="blog-list">
             <h1>{title}</h1>
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,16 +2,21 @@ import { useState, useEffect } from "react";
 import BlogList from "./Bloglist";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setIsPending] = useState(true);
 
  useEffect(()=>{
-   fetch('http://localhost:8000/blogs').then(res=>{
-    return res.json();
-   }).then(data=>{
-    setBlogs(data);
-   })
+    setTimeout(()=> {
+        fetch('http://localhost:8000/blogs').then(res=>{
+        return res.json();
+        }).then(data=>{
+        setBlogs(data);
+        setIsPending(false);
+        })
+    },1000);
  }, []);   
     return (  
         <div className="home">
+            {isLoading && <div>Loading.............</div>}
             {blogs && <BlogList blogs = {blogs} title="All Blogs" />}
         </div>
     );
