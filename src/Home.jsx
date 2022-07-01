b03701bd8ebda4e17c69c9650da8fa9fdd883d4d
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-07-01 03:21:07 +0100

    list_preview

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 const Home = () => {
-    const handleClick = (name) =>{
-        console.log(name, "Clicked me")
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
     return (  
         <div className="home">
             <h1>HomePage</h1>
-            <button onClick={() => handleClick("Viktor")}>Click me</button>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h1>{blog.title}</h1>
+                    <p>Written by {blog.author}</p>
+
+                </div>
+            ))}
         </div>
     );
 }
