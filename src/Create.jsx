1077fd21b8b6f505e8f185596b53ace3c2dfdb96
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-07-02 23:39:49 +0100

    added fetch-post to handlesubmit fxn

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,12 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState('james');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
-
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e)=>{
         e.preventDefault();
         const blog = {title,body,author};
         console.log(blog);
+        setIsPending(true);
+    fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+        
+    }).then(()=>{
+        setIsPending(false);
+        console.log("added"); 
+    })
     }
     return ( 
         <div className="create">
@@ -27,7 +37,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>  
-                <button>Add Blog</button>              
+                {!isPending  &&<button>Add Blog</button> }   
+                {isPending  &&<button disabled>Adding blog.....</button> }             
             </form>
         </div>
      );
