eefe10da26b45ad57b3be0a32c8e345fe900c71c
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-07-02 23:10:40 +0100

    added handleSubmit function to create.jsx

--- src/Create.jsx
+++ src/Create.jsx
@@ -4,15 +4,20 @@ const Create = () => {
     const [title, setTitle] = useState('james');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const blog = {title,body,author};
+        console.log(blog);
+    }
     return ( 
         <div className="create">
             <h2>Add a new blog </h2>
-            <form >
+            <form  onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input type="text" value={title} required  onChange={(e)=> setTitle(e.target.value)} />
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+             
                 <label>Blog body:</label>
                 <textarea required value={body} onChange={(e)=> setBody(e.target.value)}>
 
